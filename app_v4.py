b1a6bf6d0cbd7c5e95f5813c53b27bce9b767f58
jhpark00605-alt <jhpark00605@gmail.com>
2026-06-17 19:20:22 +0900

    Update app_v4.py

--- app_v4.py
+++ app_v4.py
@@ -1220,8 +1220,6 @@ with st.container(border=True):
                                     📈 <b>마케팅 순수 인과효과(DiD):</b> <span style='color: {style_config["text"]}; font-weight: bold;'>{row['DiD 순수 마케팅 효과']}</span>
                                 </p>
                             </div>
-                            # ... (위쪽 코드에서 이어짐) ...
-                                # 💡 통박스 내부에 완전히 갇힌 일체형 진단 디스플레이
                             <div style='
                                 margin-top: 4mm; 
                                 padding: 2.5mm; 
